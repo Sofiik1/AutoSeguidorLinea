17a63a41bfaf7976b734bbc1e6ad54b5518fa5a3
agusglusman <106568739+agusglusman@users.noreply.github.com>
2024-11-05 18:46:03 -0300

    Update auto_seguidor_linea.ino

--- auto_seguidor_linea.ino
+++ auto_seguidor_linea.ino
@@ -1,15 +1,15 @@
 #include <AFMotor.h>
 
 // Pines de los sensores
-const int ledPin1 = 52;
-const int ledPin2 = 50;
-const int ledPin3 = 48;
-const int ledPin4 = 46;
-const int ledPin5 = 44;
+const int ledPin1 = 22; //izq
+const int ledPin2 = 24;
+const int ledPin3 = 26;
+const int ledPin4 = 28;
+const int ledPin5 = 30; //der
 
 
-AF_DCMotor motor1(3);  // Motor 1 conectado a entrada 3
-AF_DCMotor motor2(4);  // Motor 2 conectado a entrada 4
+AF_DCMotor motor1(3);  // Motor 1 conectado a entrada 3 DERECHA 
+AF_DCMotor motor2(4);  // Motor 2 conectado a entrada 4 izquierda
 
 
 void setup() {
@@ -23,33 +23,55 @@ void setup() {
   pinMode(ledPin3, INPUT);
   pinMode(ledPin4, INPUT);
   pinMode(ledPin5, INPUT);
-}
 
-void loop() {
   // Lee los valores de los sensores
-  int value1 = digitalRead(ledPin1);
+  int value1 = digitalRead(ledPin1); //izq
   int value2 = digitalRead(ledPin2);
   int value3 = digitalRead(ledPin3);
   int value4 = digitalRead(ledPin4);
-  int value5 = digitalRead(ledPin5);
+  int value5 = digitalRead(ledPin5); //der
 
-  // Muestra los resultados en el monitor serial
-  Serial.print("Sensor 1: "); Serial.println(value1);
-  Serial.print("Sensor 2: "); Serial.println(value2);
-  Serial.print("Sensor 3: "); Serial.println(value3);
-  Serial.print("Sensor 4: "); Serial.println(value4);
-  Serial.print("Sensor 5: "); Serial.println(value5);
+  while (true) {
+      int value1 = digitalRead(ledPin1); // Izquierda
+      int value2 = digitalRead(ledPin2);
+      int value3 = digitalRead(ledPin3); // Centro
+      int value4 = digitalRead(ledPin4);
+      int value5 = digitalRead(ledPin5); // Derecha
 
-  // Espera un poco antes de volver a leer (pcional)
-  if(value1 == 1 || value2 == 1 || value3 == 1 || value4 == 1 || value5 == 1){
-    motor1.setSpeed(255); // Configurar la velocidad del motor (0-255)
-    motor2.setSpeed(255); // Configurar la velocidad del motor (0-255)
-    motor1.run(FORWARD);
-    motor2.run(FORWARD);
+      if (value1 == 1 && value2 == 1 && value3 == 0 && value4 == 1 && value5 == 1) {
+        delay(2000); // Espera 2 segundos
+        motor1.setSpeed(75); // Configurar velocidad inicial del motor1
+        motor2.setSpeed(75); // Configurar velocidad inicial del motor2
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        break; // Sale del bucle al iniciar el movimiento
+      }
+    }
+  
+}
+
+void loop() {
+  
+  // Lee los valores de los sensores
+  int value1 = digitalRead(ledPin1); //izq
+  int value2 = digitalRead(ledPin2);
+  int value3 = digitalRead(ledPin3);
+  int value4 = digitalRead(ledPin4);
+  int value5 = digitalRead(ledPin5); //der
+
+    // Baja la velocidad de motor1 si el sensor derecho se apaga
+  if (value4 == 0 || value5 == 0) {
+    motor1.setSpeed(10); // Disminuye la velocidad de motor1 si el sensor derecho está apagado
+    motor2.setSpeed(85);
+  } else if(value1 == 0 || value2 == 0){
+    motor1.setSpeed(85);
+    motor2.setSpeed(10);
+  } else {
+    motor1.setSpeed(40);
+    motor2.setSpeed(40);
+    delay(25);
+    motor1.setSpeed(75);
+    motor2.setSpeed(75);
   }
-  else {
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-  }
-  delay(500);  // 500 milisegundos de espera
+  
 }
